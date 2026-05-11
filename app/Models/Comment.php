6f9208de82b0ad5234003d76b2b0fa5b19d2f64d
zakkal <zaki.alghifari0306>
2026-05-11 22:06:25 +0700

    updated

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -10,7 +10,6 @@ class Comment extends Model
 {
     use HasFactory, SoftDeletes;
 
-    // Tambahkan baris ini agar Laravel izinkan simpan data
     protected $fillable = [
         'user_id',
         'opportunity_id',
@@ -18,21 +17,25 @@ class Comment extends Model
         'parent_id',
     ];
 
-    // Relasi ke User
     public function user()
     {
         return $this->belongsTo(User::class);
     }
 
-    // Relasi ke Opportunity
     public function opportunity()
     {
         return $this->belongsTo(Opportunity::class);
     }
 
-    // Relasi untuk Balasan (Replies)
+    // Relasi untuk mengambil balasan dari komentar ini
     public function replies()
     {
         return $this->hasMany(Comment::class, 'parent_id');
+    }
+
+    // Relasi balik (opsional): komentar ini balasannya siapa?
+    public function parent()
+    {
+        return $this->belongsTo(Comment::class, 'parent_id');
     }
 }
